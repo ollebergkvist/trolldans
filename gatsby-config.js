2a83939c09768b4bf49643c65dcff9e277689dd6
”Olle <ollebergkvist@gmail.com>
2022-02-05 07:27:26 -0700

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: [{
+    resolve: 'gatsby-source-contentful',
+    options: {
+      "accessToken": "hhh",
+      "spaceId": ""
+    }
+  }, {
+    resolve: 'gatsby-plugin-google-analytics',
+    options: {
+      "trackingId": "hh"
+    }
+  }, "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", {
+    resolve: 'gatsby-plugin-manifest',
+    options: {
+      "icon": "src/images/icon.png"
+    }
+  }, "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "images",
+      "path": "./src/images/"
+    },
+    __key: "images"
+  }]
+};
